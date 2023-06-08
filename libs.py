b79d77435d9eab5a0088370d4f4ddec70ba9f74c
Loco-c <carloscustodio9@gmail.com>
2023-06-08 11:12:20 +0100

    small edits

--- libs.py
+++ libs.py
@@ -1,10 +1,10 @@
 # Includes all need imports
 # pip install selenium
 # pip install webdriver-manager
-# 
+#
 # import warnings
 # warnings.filters('ignore')
-# matplotlib inline 
+# matplotlib inline
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -16,5 +16,9 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.chrome.options import Options
+
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
+driver = webdriver.Chrome(
+    executable_path='/Users/${userName}/Drivers/chromedriver', chrome_options=options)
