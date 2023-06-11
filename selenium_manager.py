7dd6dd07ce0ad52c15f39a6662e7c830109660e2
Loco-c <carloscustodio9@gmail.com>
2023-06-11 10:05:40 +0100

    Testing page function

--- selenium_manager.py
+++ selenium_manager.py
@@ -6,16 +6,16 @@
 # warnings.filters('ignore')
 # matplotlib inline
 
-# import selenium
-# import webdriver_manager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 options = webdriver.ChromeOptions()
-options.add_argument("headless")
+# options.add_argument("headless")
+options.add_argument("--disable-gpu'")
 driver = webdriver.Chrome(
     executable_path='/Users/${userName}/Drivers/chromedriver', chrome_options=options)
